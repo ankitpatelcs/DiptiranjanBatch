0bb491499e45c759c692aa1073b44ae376f92323
Ankit <ankitpatel.tops@gmail.com>
2022-10-22 11:56:06 +0530

    razorpay

--- Django/myapp/urls.py
+++ Django/myapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('products/',views.products,name='products'),
     path('addtocart/',views.addtocart,name='addtocart'),
     path('cart/',views.cart,name='cart'),
-    path('checkout/',views.checkout,name='checkout'),
+    #path('checkout/',views.checkout,name='checkout'),
     path('single/<int:pid>',views.single,name='single'),
+    path('paymenthandler/', views.paymenthandler, name='paymenthandler'),
+    path('pay/',views.checkout,name='pay'),
 ]
